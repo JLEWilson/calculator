04d47b20e4e96b74aafa77ab0b51b47981ac0dec
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-10-18 12:42:11 -0800

    Add Celsius and Farenheit conversion functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
 function add(number1, number2){
 	return number1 + number2;
 }
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
-
 function subtract(number1, number2){
 	return number1 - number2;
 }
-function twoTimes(number1, number2){
+function multiply(number1, number2){
 	return number1 * number2;
 }
-function threeTimes(number1, number2, number3)
-{
-	return number1 * number2 * number3; 
-}
-function divideTwo(number1, number2){
+function divide(number1, number2){
 	return number1 / number2;
 }
 function remainder(number1, number2){
 	return number1 % number2;
 }
+function celsiusToFahrenheit(number){
+  number *= 1.8;
+  number += 32;
+  return number;
+}
+function fahrenheitToCelsius(number){
+  number -= 32;
+  number *= .5556;
+  return number;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+//const number2 = parseInt(prompt("Enter another number:"));
+const result = (fahrenheitToCelsius(number1));
+
+alert(result);
