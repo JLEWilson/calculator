e63059e9dddc49c017731a48a3465c7140ed6e62
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-10-18 12:11:19 -0800

    Initialize project layout

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function add(number1, number2){
+	return number1 + number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
+
+function subtract(number1, number2){
+	return number1 - number2;
+}
+function twoTimes(number1, number2){
+	return number1 * number2;
+}
+function threeTimes(number1, number2, number3)
+{
+	return number1 * number2 * number3; 
+}
+function divideTwo(number1, number2){
+	return number1 / number2;
+}
+function remainder(number1, number2){
+	re
